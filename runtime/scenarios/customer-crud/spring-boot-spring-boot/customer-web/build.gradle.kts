a735463d3d0767ccc538a1035331c10565f49e00
Phuong Tran <phuong.tran@zalora.com>
2026-05-06 23:07:52 +0700

    Use runtime JVM 65b36b8ad2ec

--- runtime/scenarios/customer-crud/spring-boot-spring-boot/customer-web/build.gradle.kts
+++ runtime/scenarios/customer-crud/spring-boot-spring-boot/customer-web/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
     implementation(project(":runtime:scenarios:customer-crud:spring-boot-spring-boot:customer-contract"))
-    implementation("coakka.v2:coakka-jvm-native-runtime-v2:0.1.0-g0cb644340467-cfb8ee4")
+    implementation("coakka.v2:coakka-jvm-native-runtime-v2:0.1.0-g65b36b8ad2ec")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
