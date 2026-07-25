f62f719c7d186c23b18ad2919ceb08004292a2ea
Phuong Tran <phuong.tran@zalora.com>
2026-07-26 03:06:06 +0700

    samples: run Go doc patch tags

--- logger/go/basic/run.sh
+++ logger/go/basic/run.sh
@@ -17,7 +17,7 @@ module coakka-logger-go-basic-sample
 
 go 1.22
 
-require ${module_path} v1.2.1
+require ${module_path} v1.2.2
 EOF
 
 (
--- runtime/scenarios/customer-crud/spring-boot-go/run.sh
+++ runtime/scenarios/customer-crud/spring-boot-go/run.sh
@@ -44,7 +44,7 @@ module coakka-runtime-spring-boot-go-store
 
 go 1.23.0
 
-require ${module_path} v1.3.2
+require ${module_path} v1.3.3
 EOF
 }
 
